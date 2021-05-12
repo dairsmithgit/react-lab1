0e716b9c50682fe420741c7c9ebfb7e9e5982f8c
Dair Smith <dair.code@gmail.com>
2021-05-12 16:52:50 -0400

    ad design html

--- src/components/AdDesigner.tsx
+++ src/components/AdDesigner.tsx
@@ -3,6 +3,31 @@ import './AdDesigner.css';
 
 function AdDesigner() {
 
+
+    return (
+        <div className="AdDesigner">
+            <h1>Ad Designer</h1>
+            <div className="adBox">
+                <p>Vote For</p>
+                <p className="adFlavor"></p>
+            </div>
+            <h2>What to Support</h2>
+            <p>
+                <button>Cookies &amp; Cream</button>
+                <button>Vanilla Bean</button>
+                <button>Matcha Tea</button>
+            </p>
+            <h2>Color Theme</h2>
+            <p>
+                <button>Light</button>
+                <button>Dark</button>
+            </p>
+            <h2>Font Size</h2>
+            <button>Down</button>
+            
+            <button>Up</button>
+        </div>
+    )
 }
 
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,6 @@ import { useState } from 'react';
 import './Header.css';
 
 function Header() {
-
-
-
     return (
         <header className="Header">Ice Cream Wars</header>
     )
