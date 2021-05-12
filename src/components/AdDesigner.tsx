180c25aedb9c8fd132a25fa17cf53511d8290ebf
Dair Smith <dair.code@gmail.com>
2021-05-12 17:00:34 -0400

    ad flavor change func

--- src/components/AdDesigner.tsx
+++ src/components/AdDesigner.tsx
@@ -2,20 +2,30 @@ import { useState } from 'react';
 import './AdDesigner.css';
 
 function AdDesigner() {
+    const [ adFlavor, setFlavor ] = useState("");
 
+    function changeFlavorCookie() {
+        setFlavor("Cookies & Cream");
+    }
+    function changeFlavorVanilla() {
+        setFlavor("Vanilla Bean");
+    }
+    function changeFlavorMatcha() {
+        setFlavor("Matcha Tea");
+    }
 
     return (
         <div className="AdDesigner">
             <h1>Ad Designer</h1>
             <div className="adBox">
                 <p>Vote For</p>
-                <p className="adFlavor"></p>
+                <p className="adFlavor">{adFlavor}</p>
             </div>
             <h2>What to Support</h2>
             <p>
-                <button>Cookies &amp; Cream</button>
-                <button>Vanilla Bean</button>
-                <button>Matcha Tea</button>
+                <button onClick={changeFlavorCookie}>Cookies &amp; Cream</button>
+                <button onClick={changeFlavorVanilla}>Vanilla Bean</button>
+                <button onClick={changeFlavorMatcha}>Matcha Tea</button>
             </p>
             <h2>Color Theme</h2>
             <p>
@@ -24,7 +34,7 @@ function AdDesigner() {
             </p>
             <h2>Font Size</h2>
             <button>Down</button>
-            
+
             <button>Up</button>
         </div>
     )
