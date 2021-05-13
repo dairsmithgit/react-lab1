d0813ed551dfdfbcc89609279184ca36d75d2594
Dair Smith <dair.code@gmail.com>
2021-05-13 17:06:56 -0400

    setting up vote vars and html, needs funcs

--- src/components/Votes.tsx
+++ src/components/Votes.tsx
@@ -2,7 +2,16 @@ import { useState } from 'react';
 import './Votes.css';
 
 function Votes() {
+    const [ cookieVotes, setCookieVotes ] = useState(0);
+    const [ vanillaVotes, setVanillaVotes ] = useState(0);
+    const [ matchaVotes, setMatchaVotes ] = useState(0);
 
+    let cookieVoteNum = cookieVotes;
+    let vanillaVoteNum = vanillaVotes;
+    let matchaVoteNum = matchaVotes;
+
+
+    // buttons need onClick func to increment vote variables
     return (
         <div className="Votes">
             <h1>Vote Here</h1>
@@ -11,6 +20,12 @@ function Votes() {
                <button>Vanilla Bean</button>
                <button>Matcha Tea</button>
             </p>
+            <p><h3>Cookies &amp; Cream: </h3>{cookieVoteNum}</p>
+            <div className="CookieVotes">voting bar cookies/cream</div>
+            <p><h3>Vanilla Bean: </h3>{vanillaVoteNum}</p>
+            <div className="VanillaVotes">voting bar vanilla bean</div>
+            <p><h3>Matcha Tea: </h3>{matchaVoteNum}</p>
+            <div className="MatchaVotes">voting bar matcha tea</div>
         </div>
     )
 }
